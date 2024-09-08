3dab9a8f8b2973dd0f6c8903179db44b36f7069e
zorndog <141019996+zorndog@users.noreply.github.com>
2024-09-08 17:50:08 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pycountry',
+        'pypopulation',
+        'numpy',
+        # Add any other dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'run-analysis=mypackage.run_analysis:main',  # Adjust the command and function as needed
+        ],
+    },
+    description='A package for analyzing imdb movie data.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Kajetan Bialy',
+    url='https://github.com/zorndog/Python-Project',  # Adjust with your repo URL if available
+)
